9f90885bb5de19580ab43bc840db5a303381e45b
jonjon1322 <92115056+jonjon1322@users.noreply.github.com>
2022-07-03 18:47:31 -0400

    added javadoc

--- src/main/java/org/discordbots/api/client/io/UnsuccessfulHttpException.java
+++ src/main/java/org/discordbots/api/client/io/UnsuccessfulHttpException.java
@@ -1,7 +1,13 @@
 package org.discordbots.api.client.io;
 
 public class UnsuccessfulHttpException extends Exception {
-
+    
+    /**
+    * If the http exception is unsuccess
+    *
+    * @param code the Http code
+    * @param message the Http Exception message
+    */
     public UnsuccessfulHttpException(int code, String message) {
         super("The server responded with code: " + code + ", message: " + message);
     }
