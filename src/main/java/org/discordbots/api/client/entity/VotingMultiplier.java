84f78f93e8060144f1a54bbf3437ce78870506e4
jonjon1322 <92115056+jonjon1322@users.noreply.github.com>
2022-07-03 18:23:31 -0400

    added javadoc

--- src/main/java/org/discordbots/api/client/entity/VotingMultiplier.java
+++ src/main/java/org/discordbots/api/client/entity/VotingMultiplier.java
@@ -3,12 +3,18 @@ package org.discordbots.api.client.entity;
 import com.google.gson.annotations.SerializedName;
 
 public class VotingMultiplier {
-
+    /**
+    * If it's the weekend
+    */
     @SerializedName("is_weekend")
     private boolean weekend;
 
 
-
+    /**
+    * If it's the weekend
+    *
+    * @return boolean, the weekend
+    */
     public boolean isWeekend() {
         return weekend;
     }
