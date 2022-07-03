4211e4d2fbd40a4cb3f2ac044cc861fbeaabdef2
jonjon1322 <92115056+jonjon1322@users.noreply.github.com>
2022-07-03 18:45:46 -0400

    added javadoc

--- src/main/java/org/discordbots/api/client/impl/DiscordBotListAPIImpl.java
+++ src/main/java/org/discordbots/api/client/impl/DiscordBotListAPIImpl.java
@@ -21,18 +21,35 @@ import java.util.concurrent.CompletionStage;
 import java.util.stream.Collectors;
 
 public class DiscordBotListAPIImpl implements DiscordBotListAPI {
-
+    
+    /**
+    * The HttpUrl builder for top.gg
+    */
     private static final HttpUrl baseUrl = new HttpUrl.Builder()
             .scheme("https")
             .host("top.gg")
             .addPathSegment("api")
             .build();
-
+    
+    /**
+    * The OkHttpClient
+    */
     private final OkHttpClient httpClient;
+    /**
+    * The Gson
+    */
     private final Gson gson;
-
+    /**
+    * The token and botId
+    */
     private final String token, botId;
-
+    
+    /**
+    * The implementation of the DBLAPI
+    *
+    * @param token the bots token retrieved from the webhooks page
+    * @param botId the bots id
+    */
     public DiscordBotListAPIImpl(String token, String botId) {
         this.token = token;
         this.botId = botId;
@@ -50,7 +67,13 @@ public class DiscordBotListAPIImpl implements DiscordBotListAPI {
                 })
                 .build();
     }
-
+    /**
+    * Sets the stats of your bot
+    *
+    * @param shardId the shard id
+    * @param shardTotal the shard total
+    * @param serverCount the server count
+    */
     public CompletionStage<Void> setStats(int shardId, int shardTotal, int serverCount) {
         JSONObject json = new JSONObject()
                 .put("shard_id", shardId)
@@ -59,21 +82,33 @@ public class DiscordBotListAPIImpl implements DiscordBotListAPI {
 
         return setStats(json);
     }
-
+    /**
+    * Sets the stats of your bot
+    *
+    * @param shardServerCounts the shard server counts
+    */
     public CompletionStage<Void> setStats(List<Integer> shardServerCounts) {
         JSONObject json = new JSONObject()
                 .put("shards", shardServerCounts);
 
         return setStats(json);
     }
-
+    /**
+    * Sets the stats of your bot
+    *
+    * @param serverCount the server count
+    */
     public CompletionStage<Void> setStats(int serverCount) {
         JSONObject json = new JSONObject()
                 .put("server_count", serverCount);
 
         return setStats(json);
     }
-
+    /**
+    * Sets the stats of your bot
+    *
+    * @param jsonBody sets the bots stats from a JSON file
+    */
     private CompletionStage<Void> setStats(JSONObject jsonBody) {
         HttpUrl url = baseUrl.newBuilder()
                 .addPathSegment("bots")
@@ -83,7 +118,13 @@ public class DiscordBotListAPIImpl implements DiscordBotListAPI {
 
         return post(url, jsonBody, Void.class);
     }
-
+    /**
+    * Gets the stats of your bot
+    *
+    * @param botId the bots id
+    *
+    * @return CompletionStage<BotStats>, the bots stats
+    */
     public CompletionStage<BotStats> getStats(String botId) {
         HttpUrl url = baseUrl.newBuilder()
                 .addPathSegment("bots")
@@ -93,7 +134,13 @@ public class DiscordBotListAPIImpl implements DiscordBotListAPI {
 
         return get(url, BotStats.class);
     }
-
+    /**
+    * Gets the voters for your bot
+    *
+    * @param botId the bots id
+    *
+    * @return CompletionStage<List<SimpleUser>>, the bots voters
+    */
     public CompletionStage<List<SimpleUser>> getVoters(String botId) {
         HttpUrl url = baseUrl.newBuilder()
                 .addPathSegment("bots")
@@ -108,7 +155,13 @@ public class DiscordBotListAPIImpl implements DiscordBotListAPI {
             return Arrays.asList(arrayTransformer.transform(resp));
         });
     }
-
+    /**
+    * Gets your bot
+    *
+    * @param botId the bots id
+    *
+    * @return CompletionStage<Bot>, the bot
+    */
     public CompletionStage<Bot> getBot(String botId) {
         HttpUrl url = baseUrl.newBuilder()
                 .addPathSegment("bots")
@@ -117,15 +170,42 @@ public class DiscordBotListAPIImpl implements DiscordBotListAPI {
 
         return get(url, Bot.class);
     }
-
+    /**
+    * Gets the bot
+    *
+    * @param search A search string in the format of field: value field2: value2
+    * @param limit The amount of bots to return. Max. 500
+    * @param offset Amount of bots to skip
+    *
+    * @return CompletionStage<BotResult>, A list of bots
+    */
     public CompletionStage<BotResult> getBots(Map<String, String> search, int limit, int offset) {
         return getBots(search, limit, offset, null);
     }
-
+    /**
+    * Gets the bot
+    *
+    * @param search A search string in the format of field: value field2: value2
+    * @param limit The amount of bots to return. Max. 500
+    * @param offset Amount of bots to skip
+    * @param sort The field to sort by. Prefix with - to reverse the order
+    *
+    * @return CompletionStage<BotResult>, A list of bots
+    */
     public CompletionStage<BotResult> getBots(Map<String, String> search, int limit, int offset, String sort) {
         return getBots(search, limit, offset, sort, null);
     }
-
+    /**
+    * Gets the bot
+    *
+    * @param search A search string in the format of field: value field2: value2
+    * @param limit The amount of bots to return. Max. 500
+    * @param offset Amount of bots to skip
+    * @param sort The field to sort by. Prefix with - to reverse the order
+    * @param fields A comma separated list of fields to show
+    *
+    * @return CompletionStage<BotResult>, A list of bots
+    */
     public CompletionStage<BotResult> getBots(Map<String, String> search, int limit, int offset, String sort, List<String> fields) {
         // DBL search uses this format: field1: value1 field2: value2
         String searchString = search.entrySet().stream()
@@ -151,7 +231,13 @@ public class DiscordBotListAPIImpl implements DiscordBotListAPI {
 
         return get(urlBuilder.build(), BotResult.class);
     }
-
+    /**
+    * Gets the user
+    *
+    * @param userId the id
+    *
+    * @return CompletionStage<User>, the user
+    */
     public CompletionStage<User> getUser(String userId) {
         HttpUrl url = baseUrl.newBuilder()
                 .addPathSegment("users")
@@ -160,7 +246,13 @@ public class DiscordBotListAPIImpl implements DiscordBotListAPI {
 
         return get(url, User.class);
     }
-
+    /**
+    * If the user has voted
+    *
+    * @param userId the id
+    *
+    * @return CompletionStage<Boolean>, if voted
+    */
     public CompletionStage<Boolean> hasVoted(String userId) {
         HttpUrl url = baseUrl.newBuilder()
                 .addPathSegment("bots")
@@ -174,7 +266,11 @@ public class DiscordBotListAPIImpl implements DiscordBotListAPI {
             return json.getInt("voted") == 1;
         });
     }
-
+    /**
+    * Gets the vote multiplier
+    *
+    * @return CompletionStage<VotingMultiplier>, the multiplier
+    */
     public CompletionStage<VotingMultiplier> getVotingMultiplier() {
         HttpUrl url = baseUrl.newBuilder()
                 .addPathSegment("weekend")
@@ -182,11 +278,25 @@ public class DiscordBotListAPIImpl implements DiscordBotListAPI {
 
         return get(url, VotingMultiplier.class);
     }
-
+    /**
+    * Gets the Http request
+    *
+    * @param url the http url
+    * @param aClass the class
+    *
+    * @return <E> CompletionStage<E>, E
+    */
     private <E> CompletionStage<E> get(HttpUrl url, Class<E> aClass) {
         return get(url, new DefaultResponseTransformer<>(aClass, gson));
     }
-
+    /**
+    * Gets the Http request
+    *
+    * @param url the http url
+    * @param responseTransformer the response
+    *
+    * @return <E> CompletionStage<E>, E
+    */
     private <E> CompletionStage<E> get(HttpUrl url, ResponseTransformer<E> responseTransformer) {
         Request req = new Request.Builder()
                 .get()
@@ -199,10 +309,27 @@ public class DiscordBotListAPIImpl implements DiscordBotListAPI {
     // The class provided in this is kinda unneeded because the only thing ever given to it
     // is Void, but I wanted to make it expandable (maybe some post methods will return objects
     // in the future)
+    /**
+    * posts a Http request
+    *
+    * @param url the http url
+    * @param jsonBody the JSON file
+    * @param aClass the class
+    *
+    * @return <E> CompletionStage<E>, E
+    */
     private <E> CompletionStage<E> post(HttpUrl url, JSONObject jsonBody, Class<E> aClass) {
         return post(url, jsonBody, new DefaultResponseTransformer<>(aClass, gson));
     }
-
+    /**
+    * posts a Http request
+    *
+    * @param url the http url
+    * @param jsonBody the JSON file
+    * @param responseTransformer the response
+    *
+    * @return <E> CompletionStage<E>, E
+    */
     private <E> CompletionStage<E> post(HttpUrl url, JSONObject jsonBody, ResponseTransformer<E> responseTransformer) {
         MediaType mediaType = MediaType.parse("application/json");
         RequestBody body = RequestBody.create(mediaType, jsonBody.toString());
@@ -214,7 +341,14 @@ public class DiscordBotListAPIImpl implements DiscordBotListAPI {
 
         return execute(req, responseTransformer);
     }
-
+    /**
+    * executes a request
+    *
+    * @param request the http request
+    * @param responseTransformer the response
+    *
+    * @return <E> CompletionStage<E>, E
+    */
     private <E> CompletionStage<E> execute(Request request, ResponseTransformer<E> responseTransformer) {
         Call call = httpClient.newCall(request);
 
