0a72c1c5fb5d3f664170fb82d5e97ee15bdc1a31
jonjon1322 <92115056+jonjon1322@users.noreply.github.com>
2022-07-03 18:04:47 -0400

    added javadoc

--- src/main/java/org/discordbots/api/client/entity/Result.java
+++ src/main/java/org/discordbots/api/client/entity/Result.java
@@ -3,28 +3,54 @@ package org.discordbots.api.client.entity;
 import java.util.List;
 
 public class Result<T> {
-
+    /**
+    * the search results
+    */
     private List<T> results;
+    
+    /**
+    * the search settings
+    */
     private int limit, offset, count, total;
 
 
-
+    /**
+    * the results of the search
+    *
+    * @return List<T>, the results 
+    */
     public List<T> getResults() {
         return results;
     }
-
+    /**
+    * the limit of the search
+    *
+    * @return int, the limit
+    */
     public int getLimit() {
         return limit;
     }
-
+    /**
+    * the offset of the search
+    *
+    * @return int, the offset
+    */
     public int getOffset() {
         return offset;
     }
-
+    /**
+    * the count of items in the search
+    *
+    * @return int, the count
+    */
     public int getCount() {
         return count;
     }
-
+    /**
+    * the total of the search
+    *
+    * @return int, the total
+    */
     public int getTotal() {
         return total;
     }
