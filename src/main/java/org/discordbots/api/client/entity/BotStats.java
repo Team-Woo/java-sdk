e96330d7dd179aae374be6f3de3f4156bb90117e
jonjon1322 <92115056+jonjon1322@users.noreply.github.com>
2022-07-03 17:58:50 -0400

    added javadoc

--- src/main/java/org/discordbots/api/client/entity/BotStats.java
+++ src/main/java/org/discordbots/api/client/entity/BotStats.java
@@ -6,11 +6,27 @@ import java.util.Collections;
 import java.util.List;
 
 public class BotStats {
-
+    /**
+    * The bots server count
+    */
     @SerializedName("server_count")
     private int serverCount;
+    /**
+    * The bots shards
+    */
     private List<Integer> shards;
-
+    
+    
+    /**
+    * Gets the bots server count
+    *
+    * @return int, the server count
+    */
     public int getServerCount() { return serverCount; }
+    /**
+    * The bots shards
+    *
+    * @return List<Integer>, the shards
+    */
     public List<Integer> getShards() { return Collections.unmodifiableList(shards); }
 }
